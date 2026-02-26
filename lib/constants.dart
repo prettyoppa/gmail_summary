c3de64a6732b906bade7f499825da07abf406c4d
PRETTY <prettyoppa.k@gmail.com>
2026-02-26 09:41:46 +0900

    setting: forcefully apply qas environment settings

--- lib/constants.dart
+++ lib/constants.dart
@@ -6,9 +6,9 @@ class AppConstants {
       // main 프로젝트 웹 client Id
       // '474725837334-dr5ulbt7a59rjnv5kumon6he56n1fibu.apps.googleusercontent.com';
       // qas 프로젝트 웹 client Id
-      // '512126694471-cl8ehil75dt9htddkorufcqolets0tfi.apps.googleusercontent.com';
-      // dev 프로젝트 웹 client Id
-      '877541084240-7c8sqajvr2pk2f3uba2ejrdcr66ep46f.apps.googleusercontent.com';
+      '512126694471-cl8ehil75dt9htddkorufcqolets0tfi.apps.googleusercontent.com';
+  // dev 프로젝트 웹 client Id
+  // '877541084240-7c8sqajvr2pk2f3uba2ejrdcr66ep46f.apps.googleusercontent.com';
 
   // Gemini API 키 (사용자님의 키를 넣으세요)
   // static const String geminiApiKey = 'AIz*******FPA';
--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -42,40 +42,40 @@ class DefaultFirebaseOptions {
   // );
 
   // qas 프로젝트 firebase 설정 ;
-  // static const FirebaseOptions web = FirebaseOptions(
-  //   apiKey: 'AIzaSyCI8Vytv5Nd4XSEVn1yJn1kxpFp4VQDlhM',
-  //   appId: '1:512126694471:web:38b8de30fff03dc5c22d62',
-  //   messagingSenderId: '512126694471',
-  //   projectId: 'ireadschool-qas',
-  //   authDomain: 'ireadschool-qas.firebaseapp.com',
-  //   storageBucket: 'ireadschool-qas.firebasestorage.app',
-  //   measurementId: 'G-12QJTQW6X9',
-  // );
-
-  // static const FirebaseOptions android = FirebaseOptions(
-  //   apiKey: 'AIzaSyCI8Vytv5Nd4XSEVn1yJn1kxpFp4VQDlhM',
-  //   appId: '1:512126694471:web:38b8de30fff03dc5c22d62',
-  //   messagingSenderId: '512126694471',
-  //   projectId: 'ireadschool-qas',
-  //   storageBucket: 'ireadschool-qas.firebasestorage.app',
-  // );
-
-  // dev 프로젝트 firebase 설정 ;
   static const FirebaseOptions web = FirebaseOptions(
-    apiKey: 'AIzaSyDFKJ_TU786HeBV1ZzmoG6GGKD3FW2c0B8',
-    appId: '1:877541084240:web:5966a6754fa5f376d59555',
-    messagingSenderId: '877541084240',
-    projectId: 'ireadschool-dev',
-    authDomain: 'ireadschool-dev.firebaseapp.com',
-    storageBucket: 'ireadschool-dev.firebasestorage.app',
-    measurementId: 'G-TJBNMD02FH',
+    apiKey: 'AIzaSyCI8Vytv5Nd4XSEVn1yJn1kxpFp4VQDlhM',
+    appId: '1:512126694471:web:38b8de30fff03dc5c22d62',
+    messagingSenderId: '512126694471',
+    projectId: 'ireadschool-qas',
+    authDomain: 'ireadschool-qas.firebaseapp.com',
+    storageBucket: 'ireadschool-qas.firebasestorage.app',
+    measurementId: 'G-12QJTQW6X9',
   );
 
   static const FirebaseOptions android = FirebaseOptions(
-    apiKey: 'AIzaSyDFKJ_TU786HeBV1ZzmoG6GGKD3FW2c0B8',
-    appId: '1:877541084240:web:5966a6754fa5f376d59555',
-    messagingSenderId: '877541084240',
-    projectId: 'ireadschool-dev',
-    storageBucket: 'ireadschool-dev.firebasestorage.app',
+    apiKey: 'AIzaSyCI8Vytv5Nd4XSEVn1yJn1kxpFp4VQDlhM',
+    appId: '1:512126694471:web:38b8de30fff03dc5c22d62',
+    messagingSenderId: '512126694471',
+    projectId: 'ireadschool-qas',
+    storageBucket: 'ireadschool-qas.firebasestorage.app',
   );
-}
+
+  // dev 프로젝트 firebase 설정 ;
+//   static const FirebaseOptions web = FirebaseOptions(
+//     apiKey: 'AIzaSyDFKJ_TU786HeBV1ZzmoG6GGKD3FW2c0B8',
+//     appId: '1:877541084240:web:5966a6754fa5f376d59555',
+//     messagingSenderId: '877541084240',
+//     projectId: 'ireadschool-dev',
+//     authDomain: 'ireadschool-dev.firebaseapp.com',
+//     storageBucket: 'ireadschool-dev.firebasestorage.app',
+//     measurementId: 'G-TJBNMD02FH',
+//   );
+
+//   static const FirebaseOptions android = FirebaseOptions(
+//     apiKey: 'AIzaSyDFKJ_TU786HeBV1ZzmoG6GGKD3FW2c0B8',
+//     appId: '1:877541084240:web:5966a6754fa5f376d59555',
+//     messagingSenderId: '877541084240',
+//     projectId: 'ireadschool-dev',
+//     storageBucket: 'ireadschool-dev.firebasestorage.app',
+//   );
+// }
